bb7876faf69c8fe88e9c7d6e5fdc3c0457039c48
Mykola-Veres <veresnickolay@gmail.com>
2021-12-24 19:05:47 +0200

    add

--- src/components/Paintings.jsx
+++ src/components/Paintings.jsx
@@ -1,26 +1,25 @@
 import PropTypes from 'prop-types';
-import logo512 from "./logo512.png"
+import logo512 from './logo512.png';
 
 export default function Painting({
   url = logo512,
   title,
-  price ="unable",
-  quantity
+  price = 'unable',
+  quantity,
 }) {
-  return (   
+  return (
     <section>
       <img src={url} width="700" />
       {title && <h2>{title}</h2>}
       <div>{price}Green card</div>
       <div>USA</div>
-      <p>how many:{quantity > 10 ? "many" : "less"}</p>
+      <p>how many:{quantity > 10 ? 'many' : 'less'}</p>
     </section>
-    
-  )
+  );
 }
 Painting.propTypes = {
   url: PropTypes.string,
   title: PropTypes.string,
   price: PropTypes.number,
   quantity: PropTypes.number,
-}
+};
