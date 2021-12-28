543797e3ae2db65d8c8020ac426cc04d30ea80bc
Mykola-Veres <veresnickolay@gmail.com>
2021-12-28 14:40:18 +0200

    add-data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,15 @@ import Section from '../Section/Section';
 import { Conteiner } from './App.styled';
 import user from '../../user.json';
 import ProfileSection from '../Profaile/ProfaileSection/ProfaileSection';
+import data from '../../data.json';
+import StatisticsSection from '../Data/Section/StatisticsSection';
 
 export default function App() {
   return (
     <Conteiner>
       <Section title="Top day">{<PaitingList items={paintings} />}</Section>
       <ProfileSection user={user}></ProfileSection>
-      <></>
+      <StatisticsSection title="Upload stats" stats={data}></StatisticsSection>
       <></>
     </Conteiner>
   );
---
+++ src/components/Data/Section/StatisticsSection.jsx
@@ -0,0 +1,17 @@
+import Statistics from '../Statistics/Statistics';
+export default function StatisticsSection({ title, stats }) {
+  return (
+    <section className="statistics">
+      {title && <h2>{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(stat => (
+          <Statistics
+            key={stat.id}
+            label={stat.label}
+            percentage={stat.percentage}
+          ></Statistics>
+        ))}
+      </ul>
+    </section>
+  );
+}
